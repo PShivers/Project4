3e90320467ae03e0952e688165073b1bd4e49a2d
Paul Shivers <paulsshivers@gmail.com>
2019-06-17 13:24:46 -0400

    removed static files dir

--- pcdb_django/settings.py
+++ pcdb_django/settings.py
@@ -17,11 +17,11 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 REACT_APP_DIR = os.path.join(BASE_DIR, 'client')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(REACT_APP_DIR, 'build', 'static')
+    # os.path.join(BASE_DIR, 'static'),
+    os.path.join(REACT_APP_DIR, 'build', 'static'),
 )
 
 
