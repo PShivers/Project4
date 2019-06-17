0ba8e1b9b217ce0a191462302443e374b93f3182
Paul Shivers <paulsshivers@gmail.com>
2019-06-17 11:42:20 -0400

    removed regEx

--- pcdb_django/urls.py
+++ pcdb_django/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from . import views
  
 urlpatterns = [
-    path(r'^$', views.FrontendAppView.as_view()),
+    path('', views.FrontendAppView.as_view()),
     path('api/v1/', include('pcdb.urls')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
