4aa51f5ae42d801e7429001f6a578cf834e355a8
Paul Shivers <paulsshivers@gmail.com>
2019-06-17 11:27:15 -0400

    ran npm run build in client

---
+++ client/build/precache-manifest.12affe76daf6b964b777caa0a631fedb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebe942b888e9f25da1172085cd7cc736",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b2eab5101384d0415f8",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "23f4c3b1f2b115a6a174",
+    "url": "/static/js/2.975cff83.chunk.js"
+  },
+  {
+    "revision": "0b2eab5101384d0415f8",
+    "url": "/static/js/main.abdf2b46.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
