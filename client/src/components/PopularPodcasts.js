209d44662512a9164c92eaccecadb16fcb8c4151
Paul Shivers <paulsshivers@gmail.com>
2019-06-17 11:19:04 -0400

    styled popular podcasts some

--- client/src/components/PopularPodcasts.js
+++ client/src/components/PopularPodcasts.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 import { getPopularPodcasts } from '../util';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Podcast = styled.div`
+  margin: 5px; 
+`;
+
+const Podcasts = styled.div`
+  display:flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+`;
 
 class PopularPodCasts extends Component {
   state = {
@@ -18,18 +29,20 @@ class PopularPodCasts extends Component {
     return (
       <div>
         <h1>Popular PodCasts</h1>
-        <div>
+        <Podcasts>
           {this.state.popularPodcasts.map(podcast => {
             let linkVar = `/podcasts/${podcast.id}`;
             return (
-              <h2>
-                <li key={podcast.id}>
-                  <Link to={linkVar}>{podcast.title}</Link>
-                </li>
-              </h2>
+              <Podcast>
+                <div className="podcast" key={podcast.id}>
+                  <Link to={linkVar}>
+                    <img src={podcast.image} alt="new" />
+                  </Link>
+                </div>
+              </Podcast>
             );
           })}
-        </div>
+        </Podcasts>
       </div>
     );
   }
