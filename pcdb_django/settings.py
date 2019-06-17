d49b02d484b7328a18dbeb6a626cb2791ca75a7d
Paul Shivers <paulsshivers@gmail.com>
2019-06-17 16:53:17 -0400

    commented out stai=tic file

--- pcdb_django/settings.py
+++ pcdb_django/settings.py
@@ -17,10 +17,10 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 REACT_APP_DIR = os.path.join(BASE_DIR, 'client')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    # os.path.join(BASE_DIR, 'static'),
     os.path.join(REACT_APP_DIR, 'build', 'static'),
 )
 
